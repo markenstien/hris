91af367becee531751f06d62088dbd485e0abdac
= <gonzalesmarkangeloph@gmail.com>
2023-06-01 02:34:53 +0800

    setup update

--- app/config/setup.php
+++ app/config/setup.php
@@ -36,7 +36,7 @@
      * SYSTEM MODES
      * UP(UP AND RUNNING) , DOWN(MAINTENANCE) , DEV(DEVELOPMENT TEST DABATASE) , LOCAL (LOCAL MODE)
      */
-    $system['mode']  = 'local';
+    $system['mode']  = 'up';
 
     /**
      * SYSTEM SITE NAME
